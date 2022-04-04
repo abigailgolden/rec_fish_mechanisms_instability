cf058eff73824578ad695a135d516a8ea575c1de
abigailgolden <abigolden@gmail.com>
2022-04-04 13:22:25 -0700

    redo density-dependent catchability sensitivity analysis with angler effort scaled by mean CPUE and kur_coastal as the high-intercept/low-steepness case study

--- analysis/sensitivity_analysis_hyperstability.R
+++ analysis/sensitivity_analysis_hyperstability.R
@@ -47,22 +47,21 @@ emp_b <- betas$Bc
 
 Bmsy <- 0.00001474532
 
-
-b_li_ls <- simulate_along(par_range = b_range, params = c(0.001, 0, 0.430, 1.000, 0.02, 0.875), par_id = 4, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-b_li_hs <- simulate_along(par_range = b_range, params =  c(0.001, 0, 0.430, 1.000, 0.02, 3.5), par_id = 4, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-b_hi_ls <- simulate_along(par_range = b_range, params = c(0.001, 0, 0.430, 1.000, 0.42, 0.875), par_id = 4, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-b_hi_hs <- simulate_along(par_range = b_range, params = c(0.001, 0, 0.430, 1.000, 0.42, 3.5), par_id = 4, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
+b_kur_coastal <- simulate_along(par_range = b_range, params = param_vec, par_id = 4, utilfun = Pi_kur_coastal, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "kur_hms")
+b_kur_bottom <- simulate_along(par_range = b_range, params = param_vec, par_id = 4, utilfun = Pi_kur_bottom,Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "kur_bottom")
+b_rag_prize <- simulate_along(par_range = b_range, params = param_vec, par_id = 4, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilfun = Pi_rag_prize, utilname = "rag_prize")
+b_whi_sg <- simulate_along(par_range = b_range, params = param_vec, par_id = 4, utilfun = Pi_whi_sg, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "whi_sg")
 
 # scale their outputs relative to the reference value
 
-scaled_dat_li_ls <- scale_output(dat = b_li_ls, ref_param_val = 1)
-scaled_dat_li_hs <- scale_output(dat = b_li_hs, ref_param_val = 1)
-scaled_dat_hi_ls <- scale_output(dat = b_hi_ls, ref_param_val = 1)
-scaled_dat_hi_hs <- scale_output(dat = b_hi_hs, ref_param_val = 1)
+scaled_dat_kur_coastal <- scale_output(dat = b_kur_coastal, ref_param_val = 1)
+scaled_dat_kur_bottom <- scale_output(dat = b_kur_bottom, ref_param_val = 1)
+scaled_dat_rag_prize <- scale_output(dat = b_rag_prize, ref_param_val = 1)
+scaled_dat_whi_sg <- scale_output(dat = b_whi_sg, ref_param_val = 1)
 
 # collect all outputs in one dataframe
 
-dat_all <- rbind(scaled_dat_li_ls, scaled_dat_li_hs, scaled_dat_hi_ls, scaled_dat_hi_hs)
+dat_all <- rbind(scaled_dat_kur_coastal, scaled_dat_kur_bottom, scaled_dat_rag_prize, scaled_dat_whi_sg)
 dat_range <- dat_all %>% 
   group_by(type) %>% 
   summarize(min_val = min(val),
@@ -72,94 +71,37 @@ dat_range <- dat_all %>%
 
 # plot sensitivity analysis outputs for each angler effort function
 
-b_li_ls_plot <- outvar_heatmap(dat = scaled_dat_li_ls, 
-                                     title = "A) Low intercept, low steepness",
-                                     dat_range = dat_range,
-                                     emp_dat= emp_b,
-                                     xlab = NULL)
-
-
-b_li_hs_plot <- outvar_heatmap(dat = scaled_dat_li_hs, 
-                                 title = "B) Low intercept, high steepness",
+b_kur_coastal_plot <- outvar_heatmap(dat = scaled_dat_kur_coastal, 
+                                 title = "A) High intercept, low steepness",
                                  dat_range = dat_range,
                                  emp_dat= emp_b,
-                                 xlab = NULL, ylabelling = FALSE)
+                                 xlab = NULL)
 
-b_hi_ls_plot <- outvar_heatmap(dat = scaled_dat_hi_ls, 
-                                 title = "C) High intercept, low steepness",
-                                 dat_range = dat_range,
-                                 emp_dat= emp_b,
-                                 xlab = "Density-dependent catchability parameter beta")
 
-b_hi_hs_plot <- outvar_heatmap(dat = scaled_dat_hi_hs,
-                                 title = "D) High intercept, high steepness",
-                                 dat_range = dat_range,
-                                 emp_dat= emp_b,
-                                 xlab = "Density-dependent catchability parameter beta",
+b_kur_bottom_plot <- outvar_heatmap(dat = scaled_dat_kur_bottom, 
+                                    title = "B) High intercept, high steepness",
+                                    dat_range = dat_range,
+                                    emp_dat= emp_b,
+                                    xlab = NULL, ylabelling = FALSE)
+
+b_rag_prize_plot <- outvar_heatmap(dat = scaled_dat_rag_prize, 
+                                   title = "C) Low intercept, low steepness",
+                                   dat_range = dat_range,
+                                   emp_dat= emp_b,
+                                   xlab = "Density-dependent catchability parameter beta")
+
+b_whi_sg_plot <- outvar_heatmap(dat = scaled_dat_whi_sg,
+                                title = "D) Low intercept, high steepness",
+                                dat_range = dat_range,
+                                emp_dat= emp_b,
+                                xlab = "Density-dependent catchability parameter beta",
                                 ylabelling = FALSE)
 
-
-figname <-  "figS3_hyp_sensitivity_analysis.png"
+figname <-  paste(todaysdate, "figS3_hyp_sensitivity_analysis.png", sep = "-")
 png(paste(outfig, figname, sep = "/"), width = 12, height = 10, units = "in", res = 1000)
 
-(b_hi_ls_plot + b_hi_hs_plot) / (b_li_ls_plot + b_li_hs_plot) +plot_layout(guides = "collect")
+(b_kur_coastal_plot + b_kur_bottom_plot) / (b_rag_prize_plot + b_whi_sg_plot) +plot_layout(guides = "collect")
 
 dev.off()
 graphics.off()
 
-## Why does B > 1 produce more overfishing than B < 1 when a low intercept, high steepness effort function is used?
-
-# To answer this, simulate time series using the Whitehead et al. snapper-grouper function and hyperstable, density independent, and hyperdeplete values of beta
-
-test1<- simulate(params = c(0.001, 0, 0, 0.8, 0.02, 3.5), nsims = 100, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, ts = TRUE)
-
-test2 <- simulate(params = c(0.001, 0, 0, 1, 0.02, 3.5), nsims = 100, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, ts = TRUE)
-
-test3 <- simulate(params = c(0.001, 0, 0, 1.2, 0.02, 3.5), nsims = 100, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, ts = TRUE)
-
-
-# plot the relationships between biomass + effort and biomass + catch for each of these sets of simulations
-
-figname <- "figS4_stable_limit_cycle.png"
-png(paste(outfig, figname, sep = "/"), width = 6, height = 4, units = "in", res = 1000)
-
-par(mfrow = c(2,3))
-
-plot(test1[["Bt"]][2:200,1], test1[["Et"]][2:200,1], type = "l", xlim = c(0,0.000045), ylim = c(0,50),  xlab = "", ylab = "")
-abline(v = 0.5*Bmsy, lty= 2)
-points(test1[["Bt"]][2,1], test1[["Et"]][2,1], pch = 16, cex = 1.2)
-points(test1[["Bt"]][200,1], test1[["Et"]][200,1], pch = 17, cex = 1.2)
-title(main = "\u03b2 = 0.8", xlab = "Biomass", ylab = "Effort")
-
-plot(test2[["Bt"]][2:200,1], test2[["Et"]][2:200,1], type = "l", xlim = c(0,0.000045), ylim = c(0,50), xlab = "", ylab = "")
-abline(v = 0.5*Bmsy, lty= 2)
-points(test2[["Bt"]][2,1], test2[["Et"]][2,1], pch = 16, cex = 1.2)
-points(test2[["Bt"]][200,1], test2[["Et"]][200,1], pch = 17, cex = 1.2)
-title(main = "\u03b2 = 1", xlab = "Biomass", ylab = "")
-
-plot(test3[["Bt"]][2:200,1], test3[["Et"]][2:200,1], type = "l",  xlim = c(0,0.000045),ylim = c(0,50), xlab = "", ylab = "")
-abline(v = 0.5*Bmsy, lty= 2)
-points(test3[["Bt"]][2,1], test3[["Et"]][2,1], pch = 16, cex = 1.2)
-points(test3[["Bt"]][200,1], test3[["Et"]][200,1], pch = 17, cex = 1.2)
-title(main = "\u03b2 = 1.2", xlab = "Biomass", ylab = "")
-
-## row 2
-
-plot(test1[["Bt"]][2:200,1], test1[["Ct"]][2:200,1], type = "l", xlim = c(0,0.00004), ylim = c(0,2.5), xlab = "Biomass", ylab = "Catch")
-abline(v = 0.5*Bmsy, lty= 2)
-points(test1[["Bt"]][2,1], test1[["Ct"]][2,1], pch = 16, cex = 1.2)
-points(test1[["Bt"]][200,1], test1[["Ct"]][200,1], pch = 17, cex = 1.2)
-
-plot(test2[["Bt"]][2:200,1], test2[["Ct"]][2:200,1], type = "l", xlim = c(0,0.00004), ylim = c(0,2.5), xlab = "Biomass", ylab = "")
-abline(v = 0.5*Bmsy, lty= 2)
-points(test2[["Bt"]][2,1], test2[["Ct"]][2,1], pch = 16, cex = 1.2)
-points(test2[["Bt"]][200,1], test2[["Ct"]][200,1], pch = 17, cex = 1.2)
-
-plot(test3[["Bt"]][2:200,1], test3[["Ct"]][2:200,1], type = "l", xlim = c(0,0.00004), ylim = c(0,2.5), xlab = "Biomass", ylab = "")
-abline(v = 0.5*Bmsy, lty= 2)
-points(test3[["Bt"]][2,1], test3[["Ct"]][2,1], pch = 16, cex = 1.2)
-points(test3[["Bt"]][200,1], test3[["Ct"]][200,1], pch = 17, cex = 1.2)
-
-
-dev.off()
-graphics.off()
