3a4c7d5e3f7a1a392674659c3731dc95f26a85eb
abigailgolden <abigolden@gmail.com>
2022-04-04 14:08:40 -0700

    reran stochasticity sensitivity analysis with angler effort function scaled by mean CPUE and kur_coastal as the high intercept/low steepness case study

--- analysis/sensitivity_analysis_stochasticity.R
+++ analysis/sensitivity_analysis_stochasticity.R
@@ -8,8 +8,8 @@ source("functions/sensitivity_analysis_funs.R")
 
 library(ggpubr)
 
-param_vec <- c(0.001, 0, 0, 1.000, 0.02, 0.875)
-param_names <- c("qRec", "sd", "rho", "beta", "int", "stp")
+param_vec <- c(0.001, 0, 0, 1.000)
+param_names <- c("qRec", "sd", "rho", "beta")
 
 
 # Run analysis across a range of values of sd with no autocorrelation --------
@@ -19,19 +19,19 @@ sd_range <- seq(0, round(max_sd,1), by = 0.05)
 emp_sd <- c(0.67, 0.77, 0.74, 0.78, 0.64, 0.71, 0.74)
 
 
-nsims <- 500
+nsims <- 100
 
 Bmsy <- 0.00001474532
 
-sd_li_ls <- simulate_along(par_range = sd_range, params = c(0.001, 0, 0.430, 1.000, 0.02, 0.875), par_id = 2, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-sd_li_hs <- simulate_along(par_range = sd_range, params = c(0.001, 0, 0.430, 1.000, 0.02, 3.5), par_id = 2,  Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-sd_hi_ls <- simulate_along(par_range = sd_range, params = c(0.001, 0, 0.430, 1.000, 0.42, 0.875), par_id = 2,  Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-sd_hi_hs <- simulate_along(par_range = sd_range, params = c(0.001, 0, 0.430, 1.000, 0.42, 3.5),par_id = 2, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
+sd_kur_coastal <- simulate_along(par_range = sd_range, params = param_vec, par_id = 2, utilfun = Pi_kur_coastal, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "kur_coastal")
+sd_kur_bottom <- simulate_along(par_range = sd_range, params = param_vec, par_id = 2, utilfun = Pi_kur_bottom, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "kur_bottom")
+sd_rag_prize <- simulate_along(par_range = sd_range, params = param_vec, par_id = 2, utilfun = Pi_rag_prize,  Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "rag_prize")
+sd_whi_sg <- simulate_along(par_range = sd_range, params = param_vec, par_id = 2, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilfun = Pi_whi_sg, utilname = "whi_sg")
 
-sd_scaled_dat_li_ls <- scale_output(dat = sd_li_ls, ref_param_val = 0)
-sd_scaled_dat_li_hs <- scale_output(dat = sd_li_hs, ref_param_val = 0)
-sd_scaled_dat_hi_ls <- scale_output(dat = sd_hi_ls, ref_param_val = 0)
-sd_scaled_dat_hi_hs <- scale_output(dat = sd_hi_hs, ref_param_val = 0)
+sd_scaled_dat_kur_coastal <- scale_output(dat = sd_kur_coastal, ref_param_val = 0)
+sd_scaled_dat_kur_bottom <- scale_output(dat = sd_kur_bottom, ref_param_val = 0)
+sd_scaled_dat_rag_prize <- scale_output(dat = sd_rag_prize, ref_param_val = 0)
+sd_scaled_dat_whi_sg <- scale_output(dat = sd_whi_sg, ref_param_val = 0)
 
 
 # Run analysis across a range of values of rho with mean SD ---------------
@@ -42,29 +42,28 @@ emp_rho <- c(0.45, 0.49, 0.42, 0.46, 0.38)
 
 param_vec <- param_vec <- c(0.001, 0.74, 0, 1.000)
 
-nsims <- 500
+nsims <- 100
 
 Bmsy <- 0.00001474532
 
-rho_li_ls <- simulate_along(par_range = rho_range, params = c(0.001, 0, 0.430, 1.000, 0.02, 0.875), par_id = 3, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-rho_li_hs <- simulate_along(par_range = rho_range, params = c(0.001, 0, 0.430, 1.000, 0.02, 3.5), par_id = 3, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-rho_hi_hs <- simulate_along(par_range = rho_range, params = c(0.001, 0, 0.430, 1.000, 0.42, 0.875), par_id = 3, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
-rho_hi_ls <- simulate_along(par_range = rho_range, params = c(0.001, 0, 0.430, 1.000, 0.42, 3.5), par_id = 3, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861)
+rho_kur_coastal <- simulate_along(par_range = rho_range, params = param_vec, par_id = 3, utilfun = Pi_kur_coastal, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "kur_coastal")
+rho_kur_bottom <- simulate_along(par_range = rho_range, params = param_vec, par_id = 3, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilfun = Pi_kur_bottom, utilname = "kur_bottom")
+rho_whi_sg <- simulate_along(par_range = rho_range, params = param_vec, par_id = 3, utilfun = Pi_whi_sg, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "whi_sg")
+rho_rag_prize <- simulate_along(par_range = rho_range, params = param_vec, par_id = 3, utilfun = Pi_rag_prize, Emax = 48, Bmsy = 0.00001437875, msy = 0.3998861, utilname = "rag_prize")
 
 
-rho_scaled_dat_li_ls <- scale_output(dat = rho_li_ls, ref_param_val = 0)
-rho_scaled_dat_li_hs <- scale_output(dat = rho_li_hs, ref_param_val = 0)
-rho_scaled_dat_hi_hs <- scale_output(dat = rho_hi_hs, ref_param_val = 0)
-rho_scaled_dat_hi_ls <- scale_output(dat = rho_hi_ls, ref_param_val = 0)
+rho_scaled_dat_kur_coastal <- scale_output(dat = rho_kur_coastal, ref_param_val = 0)
+rho_scaled_dat_kur_bottom <- scale_output(dat = rho_kur_bottom, ref_param_val = 0)
+rho_scaled_dat_whi_sg <- scale_output(dat = rho_whi_sg, ref_param_val = 0)
+rho_scaled_dat_rag_prize <- scale_output(dat = rho_rag_prize, ref_param_val = 0)
 
 
 # Plot the effects of sd and rho on model outputs for Figure S2 ------------------
 
-
-dat_all <- rbind(sd_scaled_dat_li_ls, sd_scaled_dat_li_hs, 
-                 sd_scaled_dat_hi_hs, sd_scaled_dat_hi_ls,
-                 rho_scaled_dat_li_ls, rho_scaled_dat_li_hs, 
-                 rho_scaled_dat_hi_hs, rho_scaled_dat_hi_ls)
+dat_all <- rbind(sd_scaled_dat_kur_coastal, sd_scaled_dat_kur_bottom, 
+                 sd_scaled_dat_whi_sg, sd_scaled_dat_rag_prize,
+                 rho_scaled_dat_kur_coastal, rho_scaled_dat_kur_bottom, 
+                 rho_scaled_dat_whi_sg, rho_scaled_dat_rag_prize)
 
 dat_range <- dat_all %>% 
   group_by(type) %>% 
@@ -73,57 +72,57 @@ dat_range <- dat_all %>%
             min_change = min(pct_change),
             max_change = max(pct_change))
 
-sd_li_ls_plot <- outvar_heatmap(dat = sd_scaled_dat_li_ls, 
-                                  title = "A) Low intercept, low steepness",
+sd_kur_coastal_plot <- outvar_heatmap(dat = sd_scaled_dat_kur_coastal, 
+                                  title = "A) High intercept, low steepness",
                                   dat_range = dat_range,
                                   emp_dat= emp_sd,
                                   xlab = "Standard deviation of recruitment stochasticity", ylabelling = TRUE)
 
 
-sd_li_hs_plot <- outvar_heatmap(dat = sd_scaled_dat_li_hs, 
-                                     title = "B) Low intercept, high steepness",
+sd_kur_bottom_plot <- outvar_heatmap(dat = sd_scaled_dat_kur_bottom, 
+                                     title = "B) High intercept, high steepness",
                                      dat_range = dat_range,
                                      emp_dat= emp_sd,
                                      xlab = "Standard deviation of recruitment stochasticity",
                                      ylabelling = FALSE)
 
-sd_hi_ls_plot <- outvar_heatmap(dat = sd_scaled_dat_hi_ls, 
-                                    title = "C) High intercept, low steepness",
+sd_rag_prize_plot <- outvar_heatmap(dat = sd_scaled_dat_rag_prize, 
+                                    title = "C) Low intercept, low steepness",
                                     dat_range = dat_range,
                                     emp_dat= emp_sd,
                                     xlab = "Standard deviation of recruitment stochasticity")
 
-sd_hi_hs_plot <- outvar_heatmap(dat = sd_scaled_dat_hi_hs, 
-                                 title = "D) High intercept, high steepness",
+sd_whi_sg_plot <- outvar_heatmap(dat = sd_scaled_dat_whi_sg, 
+                                 title = "D) Low intercept, high steepness",
                                  dat_range = dat_range,
                                  emp_dat= emp_sd,
                                  xlab = "Standard deviation of recruitment stochasticity",
                                  ylabelling = FALSE)
 
 
-rho_li_ls_plot <- outvar_heatmap(dat = rho_scaled_dat_li_ls, 
-                                   title = "E) Low intercept, low steepness",
+rho_kur_coastal_plot <- outvar_heatmap(dat = rho_scaled_dat_kur_coastal, 
+                                   title = "E) High intercept, low steepness",
                                    dat_range = dat_range,
                                    emp_dat= emp_rho,
                                    xlab ="Autocorrelation parameter rho",
                                    ylabelling = TRUE)
 
 
-rho_li_hs_plot <- outvar_heatmap(dat = rho_scaled_dat_li_hs, 
-                                      title = "F) Low intercept, high steepness",
+rho_kur_bottom_plot <- outvar_heatmap(dat = rho_scaled_dat_kur_bottom, 
+                                      title = "F) High intercept, high steepness",
                                       dat_range = dat_range,
                                       emp_dat= emp_rho,
                                       xlab = "Autocorrelation parameter rho", 
                                       ylabelling = FALSE)
 
-rho_hi_ls_plot <- outvar_heatmap(dat = rho_scaled_dat_hi_ls, 
-                                     title = "G) High intercept, low steepness",
+rho_rag_prize_plot <- outvar_heatmap(dat = rho_scaled_dat_rag_prize, 
+                                     title = "G) Low intercept, low steepness",
                                      dat_range = dat_range,
                                      emp_dat= emp_rho,
                                      xlab = "Autocorrelation parameter rho", ylabelling = TRUE)
 
-rho_hi_hs_plot <- outvar_heatmap(dat = rho_scaled_dat_hi_hs, 
-                                  title = "H) High intercept, high steepness",
+rho_whi_sg_plot <- outvar_heatmap(dat = rho_scaled_dat_whi_sg, 
+                                  title = "H) Low intercept, high steepness",
                                   dat_range = dat_range,
                                   emp_dat= emp_rho,
                                   xlab = "Autocorrelation parameter rho",
@@ -131,13 +130,14 @@ rho_hi_hs_plot <- outvar_heatmap(dat = rho_scaled_dat_hi_hs,
 
 
 
-figname <-"figS2_stochasticity_sensitivity_analysis.png"
+figname <-paste(todaysdate, "figS2_stochasticity_sensitivity_analysis.png", sep = "-")
 png(paste(outfig, figname, sep = "/"), width = 15, height = 20, units = "in", res = 1000)
 
-(sd_hi_ls_plot | sd_hi_hs_plot) / 
-  (sd_li_ls_plot | sd_li_hs_plot) /
-  (rho_hi_ls_plot | rho_hi_hs_plot) /
-  (rho_li_ls_plot | rho_li_hs_plot)+plot_layout(guides = "collect")
+(sd_kur_coastal_plot | sd_kur_bottom_plot) / 
+  (sd_rag_prize_plot | sd_whi_sg_plot) /
+  (rho_kur_coastal_plot | rho_kur_bottom_plot) /
+  (rho_rag_prize_plot | rho_whi_sg_plot)+plot_layout(guides = "collect")
+
 
 
 dev.off()
