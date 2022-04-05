3e330859756e994b1d0a2a0ebe1919cc7896c0f3
abigailgolden <abigolden@gmail.com>
2022-04-05 11:02:15 -0700

    reformatted figure 2 to reflect new steepness values based on the difference between c60 and c50

--- fig_scripts/fig2.R
+++ fig_scripts/fig2.R
@@ -17,20 +17,20 @@ library(viridis)
 library(patchwork)
 library(gridExtra)
 library(ggpubr)
+library(lubridate)
 
 outfig <- here::here("figures")
+todaysdate <- today("EST")
 
-
-Ct <- seq(0,1, by = 0.01)
-c50 <- 0.1
+Ct <- seq(0,4, by = 0.01)
 out <- data.frame(citation = NA, species = NA, lambda = NA, intercept = NA, Ct = NA, prob = NA, funlab = NA)
 
 
 for (j in 1:length(Ct)){
   for (i in 1:nrow(df)){
-    pr = do.call(funlist[[i]], args = list(x = Ct[j], c50 = c50))
-    out[nrow(out) + 1,] = list(df[i,1], df[i,2], df[i,3],
-                               df[i,4], Ct[j], pr, names(funlist)[[i]])
+    pr = do.call(funlist[[i]], args = list(x = Ct[j]))
+    out[nrow(out) + 1,] = list(df[i,1], df[i,2], df[i,5],
+                               df[i,3], Ct[j], pr, names(funlist)[[i]])
   }
 }
 
@@ -47,24 +47,29 @@ out2 <- out %>%
   arrange(lambda) %>% 
   mutate(labs_fac = ordered(labs, unique(labs)))
 
-facet_p <- ggplot(out2, aes(x = Ct, y = prob))+
-  geom_line(aes(color = loc_labs), size = 1.5)+
-  scale_color_brewer(type = "qual", palette = "Set1", name = "Location")+
+
+col_key <- data.frame(cols = brewer.pal(5,"Set1"), 
+                      loc = locations$loc, stringsAsFactors = FALSE)
+out_manual <- left_join(out2, col_key, by = "loc")
+
+facet_p <- ggplot(out_manual, aes(x = Ct, y = prob))+
+  geom_line(aes(color = loc), size = 1.5)+
+  scale_color_manual(values = col_key$cols,
+                     name = "Location")+
   theme_classic()+
   theme(axis.line=element_line(),
-        strip.background = element_blank())+
-  scale_x_continuous(limits=c(0,1)) + scale_y_continuous(limits=c(0,1))+
+        strip.background = element_blank(),
+        legend.key.size = unit(1.5, "cm"),
+        legend.key.height = unit(1, "cm"),
+        legend.text = element_text(size = 20),
+        legend.title = element_text(size = 20))+
+  scale_x_continuous(limits=c(0,4)) + scale_y_continuous(limits=c(0,1))+
   labs(x = "Catch rate",
        y = "Proportion of max fishing effort")+
   facet_wrap(.~labs_fac, scales = "free")
 
 leg <- get_legend(facet_p)
 gg_leg <- as_ggplot(leg)
-
-cols <- brewer.pal(5,"Set1")
-col_key <- data.frame(cols = cols, loc = unique(out2$loc))
-out_manual <- left_join(out2, col_key, by = "loc")
-
 fun_nam <- names(funlist)
 
 
@@ -76,10 +81,10 @@ for (i in 1:length(funlist)){
     theme_classic()+
     theme(axis.line=element_line(),
           strip.background = element_blank(),
-          plot.title = element_text(hjust = 0.5, size = 10),
+          plot.title = element_text(hjust = 0.5, size = 12.75),
           axis.text=element_blank(),
           axis.ticks=element_blank())+
-    scale_x_continuous(limits=c(0,1)) + scale_y_continuous(limits=c(0,1))+
+    scale_x_continuous(limits=c(0,4)) + scale_y_continuous(limits=c(0,1))+
     labs(title = dat$labs,
          x = NULL,
          y = NULL)
@@ -89,22 +94,22 @@ for (i in 1:length(funlist)){
 }
 
 
-fig2 <- ggarrange(NULL, NULL, kur_inshore, kur_hms, kur_coastal, kur_bottom, NULL,
-          NULL, NULL, whi_other, NULL,  NULL, whi_mackerel, NULL,
-          rag_reef, rag_key, NULL, NULL, whi_cmp, whi_sg, whi_billfish,
-          rag_butter, rag_table, rag_prize, gent, mkwara, NULL, NULL,
-          nrow = 4, ncol = 7
+fig2 <- ggarrange(NULL, NULL, kur_inshore, kur_hms, kur_coastal, kur_bottom, NULL, NULL,
+          NULL, NULL, whi_other, NULL, NULL, whi_mackerel, NULL, whi_billfish,
+          NULL, NULL, NULL, NULL, whi_cmp, whi_sg, NULL, NULL,
+          rag_butter, rag_reef, rag_table, rag_key, rag_prize, mkwara, gent, NULL,
+          nrow = 4, ncol = 8
           )+
   annotation_custom(leg, xmin = 0.01, ymin = 0.55, xmax = 0.25)
 
 
-figname <-"fig2.png"
-png(paste(outfig, figname, sep = "/"), width = 12, height = 8, units = "in", res = 1000)
+figname <- paste(todaysdate, "fig2.png", sep = "-")
+png(paste(outfig, figname, sep = "/"), width = 17, height = 9, units = "in", res = 1000)
 
 annotate_figure(fig2,
-                bottom = text_grob(expression(atop("Catch rate", bolditalic("Increasing \u03bb")))),
+                bottom = text_grob(expression(atop("Catch rate", bolditalic("Increasing \u03bb"))), size = 16),
                 left = text_grob(expression(atop(bolditalic("Increasing intercept"), "Proportion of maximum fishing effort")), 
-                                 rot = 90))
+                                 rot = 90, size = 16))
 
 
 grid.lines(x = unit(c(0.1,0.95), 'npc'),
@@ -117,6 +122,7 @@ grid.lines(x = 0.02,
            gp = gpar(col = "black"),
            arrow = arrow(angle = 30, length = unit(0.25, "inches"),
                          ends = "last", type = "open"))
+
 
 dev.off()
 graphics.off()
